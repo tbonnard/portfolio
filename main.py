a7f4de3b76bd4764905102d970bd39b71e6bbbea
= <=>
2021-12-31 13:44:20 -0500

    port

--- main.py
+++ main.py
@@ -17,12 +17,11 @@ to_email = os.environ.get('to_email')
 smtp_url = os.environ.get('smtp_url')
 user_stmp = os.environ.get('user_stmp')
 pwd_stmp = os.environ.get('pwd_stmp')
-port_stmp = os.environ.get('port_stmp')
 
 
 def send_email(name, message):
     message_to_send = f"Subject:Portfolio: message de {name}! \n\n{message}"
-    with smtplib.SMTP(smtp_url, port=port_stmp) as connection:
+    with smtplib.SMTP(smtp_url, port=587) as connection:
         connection.login(user=user_stmp, password=pwd_stmp)
         connection.sendmail(from_addr=my_email, to_addrs=to_email, msg=message_to_send.encode(encoding='UTF-8'))
 
